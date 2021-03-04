aa84628d7d603415c343717010ff55c806def515
ValdemarF20 <valdemarfoster.dk@gmail.com>
2021-03-05 00:59:40 +0100

    Fixes

--- src/main/java/net/arcticforestmc/map/Map.java
+++ src/main/java/net/arcticforestmc/map/Map.java
@@ -4,10 +4,14 @@ import net.arcticforestmc.map.command.ConfigReload;
 import net.arcticforestmc.map.command.MapCommand;
 import net.arcticforestmc.map.libs.Yaml;
 import net.arcticforestmc.map.libs.YamlFactory;
+
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.UUID;
 
 public final class Map extends JavaPlugin {
     protected static Yaml yaml;
+    protected static UUID uuid;
 
     @Override
     public void onEnable() {
@@ -15,7 +19,7 @@ public final class Map extends JavaPlugin {
         yaml = new YamlFactory(JavaPlugin.getPlugin(Map.class)).setDefaultPathways();
         yaml.reload();
 
-        getCommand("map").setExecutor(new MapCommand(this, yaml));
+        getCommand("map").setExecutor(new MapCommand(this, yaml, uuid));
         getCommand("mapreload").setExecutor(new ConfigReload(yaml));
 
         saveDefaultConfig();
